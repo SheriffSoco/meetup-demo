11e5e046385447c6f40068a4959f664c54376793
SheriffSoco <japenes@gmail.com>
2022-11-20 22:23:11 -0800

    Finally connected a database!

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -2,10 +2,19 @@ import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
   function addMeetupHandler(meetupData) {
-    
+    fetch(
+      "https://react-getting-started-6ac84-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+      },
+    );
   }
-  
-    return (
+
+  return (
     <section>
       <h1>Create New Meetup</h1>
       <NewMeetupForm onAddMeetup={addMeetupHandler} />
