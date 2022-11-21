ca0d52e65de0fe94cc913f18d4ff3a486d2cf2bb
SheriffSoco <japenes@gmail.com>
2022-11-20 23:54:37 -0800

    Create favorites context object

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,5 +1,18 @@
+import { useContext } from "react";
+import MeetupList from "../components/meetups/MeetupList";
+
+import FavoritesContext from "../store/favorites-context";
+
 function FavoritesPage() {
-    return <div>Favorites Page!</div>;
+  const favoritesCtx = useContext(FavoritesContext);
+
+  return (
+    <section>
+      <h1>Favorites Page!</h1>
+      <p>Total favorites: <b>{favoritesCtx.totalFavorites}</b></p>
+      <MeetupList meetups={favoritesCtx.favorites} />
+    </section>
+  );
 }
 
 export default FavoritesPage;
