5d610ee449309f6731fc900e0492899203d3ace5
SheriffSoco <japenes@gmail.com>
2022-11-20 17:33:55 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import AllMeetupsPage from "./components/AllMeetups";
+import NewMeetupPage from "./components/NewMeetup";
+import FavoritesPage from "./components/Favorites";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route exact path="/" element={<AllMeetupsPage />}></Route>
+        <Route path="/new-meetup" element={<NewMeetupPage />}></Route>
+        <Route path="/favorites" element={<FavoritesPage />}></Route>
+      </Routes>
     </div>
   );
 }
