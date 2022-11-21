f1098b2929cfa162379cba8c12dc42b2310d55a5
SheriffSoco <japenes@gmail.com>
2022-11-20 21:41:17 -0800

    Add layout components

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,19 +1,22 @@
 import classes from "./MeetupItem.module.css";
+import Card from "../ui/Card";
 
 function MeetupItem(props) {
   return (
     <li key={props.meetup.id} className={classes.item}>
-      <div className={classes.image}>
-        <img src={props.meetup.image} alt={props.meetup.title} />
-      </div>
-      <div className={classes.content}>
-        <h3>{props.meetup.title}</h3>
-        <address>{props.meetup.address}</address>
-        <p>{props.meetup.description}</p>
-      </div>
-      <div className={classes.actions}>
-        <button>To Favorites</button>
-      </div>
+      <Card>
+        <div className={classes.image}>
+          <img src={props.meetup.image} alt={props.meetup.title} />
+        </div>
+        <div className={classes.content}>
+          <h3>{props.meetup.title}</h3>
+          <address>{props.meetup.address}</address>
+          <p>{props.meetup.description}</p>
+        </div>
+        <div className={classes.actions}>
+          <button>To Favorites</button>
+        </div>
+      </Card>
     </li>
   );
 }
